5a81ea501f48001b1ad63a9b45c1a8d218612002
Eli Richmond <eerichmond@gmail.com>
2024-02-22 11:35:41 -0800

    chore: add state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [thesis, setThesis] = useState();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full h-full flex gap-2">
