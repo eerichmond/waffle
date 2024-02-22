cc7ce37aa6d5b20353e0330dc92c78ec15331f17
Eli Richmond <eerichmond@gmail.com>
2024-02-22 12:14:57 -0800

    feat: convex api setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,28 @@
 import Image from "next/image";
 import { useState } from "react";
 
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
 export default function Home() {
   const [thesis, setThesis] = useState('');
   const [thoughts, setThoughts] = useState('');
 
+  const samples = useQuery(api.waffles.get);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>SAMPLES: {samples}</div>
       <div className="w-full h-full flex gap-2">
         <div className="w-3/4">
 
-          <textarea className="text-black bg-white w-full h-40" placeholder="Enter your thesis here" onChange={(event) => setThesis(event.target.value)}>{thesis}</textarea>
+          <textarea className="text-black bg-white w-full h-40" placeholder="Enter your thesis here" onChange={(event) => setThesis(event.target.value)} value={thesis}></textarea>
           <textarea
             className="min-h-screen w-full text-black bg-white"
             placeholder="Enter your thoughts here"
             onChange={(event) => setThoughts(event.target.value)}
-          >{thoughts}</textarea>
+            value={thoughts}
+          ></textarea>
         </div>
         <div className="w-1/4">-- Result here --</div>
       </div>
