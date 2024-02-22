b7642ac39ca8720f8c170fa6d09e1964d016ccda
luisarevalo21 <arevaloluis89@yahoo.com>
2024-02-22 14:50:48 -0800

    refactored component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,17 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import Image from "next/image";
+import { useState, useEffect, useRef, useMemo } from "react";
 
 import { useQuery } from "convex/react";
-import sanitizeHtml from "sanitize-html";
 
 import { api } from "../convex/_generated/api";
-import { colorBySimilarity, getSimilarity } from "./utils";
+import { colorBySimilarity } from "./utils";
 import ContentEditable from "./ContentEditableComponent";
 export default function Home() {
   const [thesis, setThesis] = useState("");
   const [thoughts, setThoughts] = useState("");
-  // const [styledThoughts, setStyledThoughts] = useState("<div></div>");
   const sentenceSimilarityCache = useRef<Record<string, number>>({});
-
-  // useEffect(() => {
-  //   const colorAll = async (str: string) => {
-  //     const split = str.split(".");
-  //     const styled = [];
-  //     for (let i = 0; i < split.length; i++) {
-  //       let similarity = 0;
-  //       if (sentenceSimilarityCache.current[split[i]]) {
-  //         similarity = sentenceSimilarityCache.current[split[i]];
-  //       } else {
-  //         similarity = await getSimilarity(thesis, split[i]);
-  //         sentenceSimilarityCache.current[split[i]] = similarity;
-  //         console.log("cache", sentenceSimilarityCache.current);
-  //       }
-  //       console.log(split[i], similarity);
-  //       styled.push(colorBySimilarity(split[i], similarity));
-  //     }
-  //     setStyledThoughts(styled.join(". "));
-  //   };
-
-  //   const endings = [".", " ", "\n", ";"];
-  //   if (endings.includes(thoughts[thoughts.length - 1]) || thoughts[thoughts.length - 1]?.trim() == "") {
-  //     console.log("coloring");
-  //     colorAll(thoughts);
-  //   }
-  // }, [thoughts, thesis]);
-
-  const maybeColoredThoughts = useMemo(() => {
-    const split = thoughts.split(".");
-
-    const sentences = [];
-    for (let i = 0; i < split.length; i++) {
-      if (sentenceSimilarityCache.current[split[i]]) {
-        const similarity = sentenceSimilarityCache.current[split[i]];
-        sentences.push(colorBySimilarity(split[i], similarity));
-      } else {
-        sentences.push(split[i]);
-      }
-    }
-
-    return sentences.join(".");
-  }, [thoughts, thesis, sentenceSimilarityCache.current]);
 
   useEffect(() => {
     sentenceSimilarityCache.current = {};
